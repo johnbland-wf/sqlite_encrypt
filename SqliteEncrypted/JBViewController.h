494c88274a6b8381a56eb87f7f55cd0ee18753bd
John C. Bland II <john.bland@webfilings.com>
2014-02-19 10:30:48 -0600

    Initial Commit

---
+++ SqliteEncrypted/JBViewController.h
@@ -0,0 +1,13 @@
+//
+//  JBViewController.h
+//  SqliteEncrypted
+//
+//  Created by John C. Bland II on 2/19/14.
+//  Copyright (c) 2014 WebFilings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JBViewController : UIViewController
+
+@end
---
+++ SqliteEncrypted/JBViewController.m
@@ -0,0 +1,29 @@
+//
+//  JBViewController.m
+//  SqliteEncrypted
+//
+//  Created by John C. Bland II on 2/19/14.
+//  Copyright (c) 2014 WebFilings. All rights reserved.
+//
+
+#import "JBViewController.h"
+
+@interface JBViewController ()
+
+@end
+
+@implementation JBViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
